46c983183f6d8680132b7ae9dbae173b5c1707f8
Tosin <me@tosinsoremekun.com>
2021-07-18 20:19:59 +0300

    added gnss spoofing script

---
+++ spoof-gnss/index.js
@@ -0,0 +1,14 @@
+const mqtt = require('mqtt');
+const faker = require('faker');
+
+require('dotenv').config();
+const client = mqtt.connect(process.env.MQTT_BROKER);
+
+client.on('connect', () => {
+    const location = {
+        lat: faker.address.latitude(),
+        long: faker.address.longitude()
+    }
+    console.log(location);
+    client.publish('transport/location', JSON.stringify(location));
+});
